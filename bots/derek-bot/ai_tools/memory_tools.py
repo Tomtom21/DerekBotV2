389e7963b7ec7d1f38c2f617534c5704c44a4d45
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2025-06-22 00:18:57 -0400

    Add save memory tool

---
+++ bots/derek-bot/ai_tools/memory_tools.py
@@ -0,0 +1,31 @@
+from shared.data_manager import DataManager
+from shared.time_utils import get_est_iso_date
+import logging
+from discord import Member
+
+class MemoryTools:
+    def __init__(self, db_manager):
+        self.db_manager = db_manager
+
+    def save_memory(self, memory_text: str, user: Member):
+        """
+        Saves a memory to the chat_memories table in the database.
+        
+        :param memory_text: The memory text to save
+        :param user_id: The ID of the user saving the memory
+        :return: Text stating whether the memory was saved or not
+        """
+        self.db_manager.ensure_user_exists(user)
+        successfully_added = self.db_manager.add_table_data(
+            table_name="chat_memories",
+            json_data={
+                "memory": memory_text,
+                "created": get_est_iso_date(),
+                "added_by": user.id
+            }
+        )
+        if successfully_added:
+            logging.info(f"User {user.name} saved memory: {memory_text}")
+            return "Memory successfully saved."
+        else:
+            return "Failed to save memory."
