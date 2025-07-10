057548ac433496bab20a03f8a9841f9ba93d4d3b
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2025-07-09 23:14:59 -0400

    Adding more languages

--- shared/constants.py
+++ shared/constants.py
@@ -1,6 +1,10 @@
 GOOGLE_TTS_VOICE_INFO = {
-    "english-US": {
+    "english-US-Leda": {
         "language_code": "en-US",
         "voice_name": "en-US-Chirp3-HD-Leda"
+    },
+    "english-US-Sadachbia": {
+        "language_code": "en-US",
+        "voice_name": "en-US-Chirp3-HD-Sadachbia"
     }
 }
