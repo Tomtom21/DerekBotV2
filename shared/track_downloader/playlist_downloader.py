84d4b07ca73b49f269cb26a1a0783300b9f209b8
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2025-08-05 22:01:27 -0400

    Refactor playlist downloader method signatures

--- shared/track_downloader/playlist_downloader.py
+++ shared/track_downloader/playlist_downloader.py
@@ -6,22 +6,33 @@ class PlaylistDownloader:
     def __init__(self, spotify_api: SpotifyAPI, youtube_api: YoutubeAPI, song_downloader: SongDownloader):
         self.song_downloader = song_downloader
 
-    def _route_playlist_download(self, playlist_url, callback):
+    async def download_playlist_by_url(self, playlist_url, callback_func, **kwargs):
+        """
+        User-callable function that downloads a playlist using a URL
+        
+        :param playlist_url: The playlist url
+        :param callback_func: The callback function to call when each song is downloaded successfully
+        :param kwargs: Additional keyword arguments
+        """
+
+    def _route_playlist_download(self, playlist_url, callback_func, **kwargs):
         """
         Routes a playlist url to one of the playlist downloading methods, calls callback for each download
 
         :param playlist_url: The playlist url
         :param callback: The callback function to call when each song is downloaded successfully
+        :param kwargs: Additional keyword arguments
         :return:
         """
         pass
 
-    def _download_spotify_playlist(self, spotify_playlist_url, callback):
+    def _download_spotify_playlist(self, spotify_playlist_url, callback_func, **kwargs):
         """
         Downloads a Spotify playlist, calls callback
 
         :param spotify_playlist_url: The Spotify playlist url
         :param callback: The callback function to call when each song is downloaded successfully
+        :param kwargs: Additional keyword arguments
         :return:
         """
         # Ensuring we're authed. We auth each call to
@@ -31,12 +42,13 @@ class PlaylistDownloader:
 
         pass
 
-    def _download_youtube_playlist(self, youtube_playlist_url, callback):
+    def _download_youtube_playlist(self, youtube_playlist_url, callback_func, **kwargs):
         """
         Downloads a YouTube playlist, calls callback
 
         :param youtube_playlist_url: The YouTube playlist url
         :param callback: The callback function to call when the song is downloaded
+        :param kwargs: Additional keyword arguments
         :return:
         """
         pass
