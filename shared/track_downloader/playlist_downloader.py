190e0af9fd140cf2933ac7fb2d6c0cfb5657c9f7
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2025-09-01 01:25:42 -0400

    Remove unused playlist download methods

--- shared/track_downloader/playlist_downloader.py
+++ shared/track_downloader/playlist_downloader.py
@@ -14,41 +14,4 @@ class PlaylistDownloader:
         :param callback_func: The callback function to call when each song is downloaded successfully
         :param kwargs: Additional keyword arguments
         """
-
-    def _route_playlist_download(self, playlist_url, callback_func, **kwargs):
-        """
-        Routes a playlist url to one of the playlist downloading methods, calls callback for each download
-
-        :param playlist_url: The playlist url
-        :param callback: The callback function to call when each song is downloaded successfully
-        :param kwargs: Additional keyword arguments
-        :return:
-        """
-        pass
-
-    def _download_spotify_playlist(self, spotify_playlist_url, callback_func, **kwargs):
-        """
-        Downloads a Spotify playlist, calls callback
-
-        :param spotify_playlist_url: The Spotify playlist url
-        :param callback: The callback function to call when each song is downloaded successfully
-        :param kwargs: Additional keyword arguments
-        :return:
-        """
-        # Ensuring we're authed. We auth each call to
-
-        # This needs to accept a list of song requests??
-        # essentially we need the list of songs to be pulled first
-
-        pass
-
-    def _download_youtube_playlist(self, youtube_playlist_url, callback_func, **kwargs):
-        """
-        Downloads a YouTube playlist, calls callback
-
-        :param youtube_playlist_url: The YouTube playlist url
-        :param callback: The callback function to call when the song is downloaded
-        :param kwargs: Additional keyword arguments
-        :return:
-        """
         pass
