4ebe62c0890a0aa15d4622bc7e4f191010cf8755
Tomtom21 <50067126+Tomtom21@users.noreply.github.com>
2025-09-01 02:15:17 -0400

    Implement async playlist download with callbacks
    Added asynchronous downloading of playlist items using a PlaylistRequest model. Each song is downloaded and a callback is invoked upon completion. All downloads are executed concurrently and awaited.

--- shared/track_downloader/playlist_downloader.py
+++ shared/track_downloader/playlist_downloader.py
@@ -1,12 +1,16 @@
+import asyncio
+import logging
+
 from shared.spotify_api import SpotifyAPI
 from shared.youtube_api import YoutubeAPI
 from shared.track_downloader.song_downloader import SongDownloader
+from shared.track_downloader.models import PlaylistRequest
 
 class PlaylistDownloader:
     def __init__(self, spotify_api: SpotifyAPI, youtube_api: YoutubeAPI, song_downloader: SongDownloader):
         self.song_downloader = song_downloader
 
-    async def download_playlist_by_url(self, playlist_url, callback_func, **kwargs):
+    async def download_playlist_by_url(self, playlist_request: PlaylistRequest, callback_func, **kwargs):
         """
         User-callable function that downloads a playlist using a URL
         
@@ -14,4 +18,16 @@ class PlaylistDownloader:
         :param callback_func: The callback function to call when each song is downloaded successfully
         :param kwargs: Additional keyword arguments
         """
-        pass
+        # Calling the callback to update the user-facing message and add it to the queue
+        async def download_and_callback(song):
+            result = await self.song_downloader.download_song_by_url(song.url)
+            await callback_func(song, result)
+
+        # Queueing up the downloads
+        download_tasks = [
+            asyncio.create_task(download_and_callback(song))
+            for song in playlist_request.items
+        ]
+
+        # Waiting for all downloads to complete
+        await asyncio.gather(*download_tasks)
